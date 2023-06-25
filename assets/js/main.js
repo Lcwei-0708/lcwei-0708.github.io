e4634251a7ae9785b03a6cdc11c7668e4c3ace31
ChengWei <lcwei0708@gmail.com>
2023-06-25 22:04:07 +0800

    2023/06/25

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,4 +267,17 @@
    */
   new PureCounter();
 
+  // 獲取當前年份
+  var currentYear = new Date().getFullYear();
+
+  // 設定出生年份
+  var birthYear = 2000;
+
+  // 計算年齡
+  var age = currentYear - birthYear;
+
+  // 將年齡放入 HTML 中
+  var ageElement = document.getElementById("age");
+  ageElement.textContent = age;
+
 })()
