6db8b2a159a3121435a01e1254aebad59fb46dfb
ChengWei <lcwei0708@gmail.com>
2023-09-17 23:13:38 +0800

    週日 2023/09/17

--- assets/js/codeBlock.js
+++ assets/js/codeBlock.js
@@ -2,30 +2,32 @@ document.addEventListener('DOMContentLoaded', function() {
   hljs.highlightAll();
   hljs.initLineNumbersOnLoad();
 
+  /*--------------------------------------------------------------
+  #   CopyButton
+  --------------------------------------------------------------*/
+
   // 獲取所有的複製按鈕和代碼元素
   const copy_btns = document.querySelectorAll('.copy');
 
   // 為每個複製按鈕添加點擊事件
   copy_btns.forEach((copy_btn, index) => {
-    copy_btn.id = `copy_btn_${index}`;
     copy_btn.addEventListener('click', (e) => {
       const parentCard = e.target.closest('.card');
-      const parentContainer = parentCard.closest('.block-container');
-      let code_element;
+      const codeElement = parentCard.querySelector('pre.code-wrap:not(.hidden) code');
 
-      if (parentContainer) {
-        const currentLanguage = parentCard.querySelector('.languageSelected').getAttribute('language');
-        code_element = parentCard.querySelector(`.code[language="${currentLanguage}"] code`);
+      if (codeElement) {
+        const textToCopy = codeElement.innerText;
+        copyToClipboard(textToCopy, copy_btn);
       }
       else {
-        code_element = parentCard.querySelector(`.code code`);
-      }      
+        console.log("error")
+      }
 
       // 禁用按鈕
       copy_btn.disabled = true;
 
       // 取得代碼的所有行
-      const lines = code_element.innerText.split('\n');
+      const lines = codeElement.innerText.split('\n');
 
       // 多行代碼，去掉行號並且去掉每行的首字符空格
       const cleanedLines = lines.filter((line, index) => index % 2 === 0).map(line => line.replace(/^\t/, ''));
@@ -55,6 +57,10 @@ document.addEventListener('DOMContentLoaded', function() {
       copy_btn.disabled = false; // 啟用按鈕
     }, 1000); // 1 秒後隱藏
   }
+
+  /*--------------------------------------------------------------
+  #   LanguageSelection
+  --------------------------------------------------------------*/
 
   // 獲取所有語言選擇器元素
   const languageSelectedList = document.querySelectorAll('.languageSelected');
@@ -112,7 +118,6 @@ document.addEventListener('DOMContentLoaded', function() {
       // 從 .languageSelector 中找到 .languageSelected
       var languageSelected = parentElement.querySelector('.languageSelected');
 
-      languageSelected.setAttribute('language', selectedLanguage);
       languageSelected.innerHTML = selectedLanguage + '<i class="bi bi-chevron-down"></i>';
     }
 
@@ -132,6 +137,49 @@ document.addEventListener('DOMContentLoaded', function() {
   // 為所有語言選擇器綁定點擊事件
   languageSelectedList.forEach((element) => {
     element.addEventListener('click', handleLanguageSelectorClick);
+  });
+
+  /*--------------------------------------------------------------
+  #   PageSelection
+  --------------------------------------------------------------*/
+
+  const pageOptions = document.querySelectorAll('.pageOptions li');
+
+  pageOptions.forEach(option => {
+    option.addEventListener('click', function() {
+      pageOptions.forEach(otherOption => {
+        if (otherOption !== option) {
+          otherOption.classList.remove('selected');
+        }
+      });
+
+      option.classList.add('selected');
+
+      const selectedPage = option.getAttribute('page');
+
+      const parentBlockContainer = option.closest('.block-container');
+      const codeBlocks = parentBlockContainer.querySelectorAll('pre');
+      const annotationBlocks = parentBlockContainer.querySelectorAll('p');
+
+      // 顯示對應的程式碼和註釋，隱藏其他的
+      codeBlocks.forEach((code) => {
+        const page = code.getAttribute('page');
+        if (page === selectedPage) {
+          code.classList.remove('hidden');
+        } else {
+          code.classList.add('hidden');
+        }
+      });
+
+      annotationBlocks.forEach((annotation) => {
+        const page = annotation.getAttribute('page');
+        if (page === selectedPage) {
+          annotation.classList.remove('hidden');
+        } else {
+          annotation.classList.add('hidden');
+        }
+      });
+    });
   });
   
 });
