0845a2e1b0d3375b116afb0800951e7fc5a4c560
ChengWei <lcwei0708@gmail.com>
2023-09-20 01:00:56 +0800

    週三 2023/09/20

--- assets/js/math.js
+++ assets/js/math.js
@@ -19,49 +19,57 @@ document.addEventListener("DOMContentLoaded", function() {
   var customAlertCopy = document.getElementById('custom-alert-copy');
   var customAlertClose = document.getElementById('custom-alert-close');
 
-  // 點擊任何 math-container 元素觸發警告框
-  mathContainers.forEach(function(mathContainer) {
-    mathContainer.addEventListener('dblclick', function() {
-      var latex = mathContainer.getAttribute('data-latex');
-      mathFormula.innerHTML = latex;
-      customAlertOverlay.style.display = 'flex';
-      document.body.style.overflow = 'hidden';
+  if(mathContainers) {
+    // 點擊任何 math-container 元素觸發警告框
+    mathContainers.forEach(function(mathContainer) {
+      mathContainer.addEventListener('dblclick', function() {
+        var latex = mathContainer.getAttribute('data-latex');
+        mathFormula.innerHTML = latex;
+        customAlertOverlay.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+      });
     });
-  });
+  }  
 
-  // 點擊複製按鈕複製公式
-  customAlertCopy.addEventListener('click', function() {
-    // 禁用複製按鈕
-    customAlertCopy.disabled = true;
+  if(customAlertCopy) {
+    // 點擊複製按鈕複製公式
+    customAlertCopy.addEventListener('click', function() {
+      // 禁用複製按鈕
+      customAlertCopy.disabled = true;
 
-    var range = document.createRange();
-    range.selectNode(mathFormula);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
+      var range = document.createRange();
+      range.selectNode(mathFormula);
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+      document.execCommand('copy');
+      window.getSelection().removeAllRanges();
 
-    // 切換 icon 過一秒再切回來
-    var originalIcon = customAlertCopy.innerHTML;
-    customAlertCopy.innerHTML = '<iconify-icon icon="tabler:check"></iconify-icon>';
-    setTimeout(function() {
-      customAlertCopy.innerHTML = originalIcon;
-      // 啟用複製按鈕
-      customAlertCopy.disabled = false;
-    }, 1000);
-  });
+      // 切換 icon 過一秒再切回來
+      var originalIcon = customAlertCopy.innerHTML;
+      customAlertCopy.innerHTML = '<iconify-icon icon="tabler:check"></iconify-icon>';
+      setTimeout(function() {
+        customAlertCopy.innerHTML = originalIcon;
+        // 啟用複製按鈕
+        customAlertCopy.disabled = false;
+      }, 1000);
+    });
+  }
 
-  // 點擊關閉按鈕關閉警告框
-  customAlertClose.addEventListener('click', function() {
-    customAlertOverlay.style.display = 'none';
-    document.body.style.overflow = 'auto';
-  });
-
-  // 點擊警告框外的區域關閉警告框
-  customAlertOverlay.addEventListener('click', function(event) {
-    if (event.target === customAlertOverlay) {
+  if(customAlertClose) {
+    // 點擊關閉按鈕關閉警告框
+    customAlertClose.addEventListener('click', function() {
       customAlertOverlay.style.display = 'none';
       document.body.style.overflow = 'auto';
-    }
-  });
+    });
+  }
+
+  if(customAlertOverlay) {
+    // 點擊警告框外的區域關閉警告框
+    customAlertOverlay.addEventListener('click', function(event) {
+      if (event.target === customAlertOverlay) {
+        customAlertOverlay.style.display = 'none';
+        document.body.style.overflow = 'auto';
+      }
+    });
+  }
 });
